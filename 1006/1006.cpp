3ede4f23e09be5affc8ab013fadd9ffecc1c3895
Assassin <littlesevenmo@gmail.com>
2018-03-06 00:34:56 +0800

    Code V1.0

---
+++ 1006/1006.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#pragma warning(disable:4996)
+
+using namespace std;
+int m;								//MÊÇÈËÊý
+string in = "24:00:00", out = "00:00:00";
+string first, last;
+
+void solution()
+{
+	string temp, name;
+	freopen("1.txt", "r", stdin);
+	cin >> m;
+	for (int i = 0; i < m; i++)
+	{
+		cin >> name;
+		//name.push_back(temp);
+		cin >> temp;
+		if (temp.compare(in) < 0)
+		{
+			in = temp;
+			first = name;
+		}
+		cin >> temp;
+		if (temp.compare(out) > 0)
+		{
+			out = temp;
+			last = name;
+		}
+	}
+	cout << first << ' ' << last;
+
+}
+
+int main(void)
+{
+	solution();
+	system("pause");
+	return 0;
+}
